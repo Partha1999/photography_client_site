9e44bcb3c2c68903aee108f7d5e254e329f4dd48
Partha1999 <impartha1999@gmail.com>
2022-12-22 21:59:59 +0600

    blog edited

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -52,7 +52,41 @@ const Blog = () => {
               <summary className="px-4 py-6 focus:outline-none focus-visible:ring-blue-600">
                 What is the difference between javascript and NodeJS?
               </summary>
-              
+              <p className="px-4 py-6 pt-0 ml-4 -mt-4 text-gray-600">
+                1. JavaScript is a client-side scripting language that is
+                lightweight, cross-platform, and interpreted. Both Java and HTML
+                include it. Node.js, on the other hand, is a V8-based
+                server-side programming language. As a result, it is used to
+                create network-centric applications. It's a networked system
+                made for data-intensive real-time applications. If we compare
+                node js vs. python, it is clear that node js will always be the
+                preferred option. 2. JavaScript is a simple programming language
+                that can be used with any browser that has the JavaScript Engine
+                installed. Node.js, on the other hand, is an interpreter or
+                execution environment for the JavaScript programming language.
+                It requires libraries that can be conveniently accessed from
+                JavaScript programming to be more helpful. 3. Any engine may run
+                JavaScript. As a result, writing JavaScript is incredibly easy,
+                and any working environment is similar to a complete browser.
+                Node.js, on the other hand, only enables the V8 engine. Written
+                JavaScript code, on the other hand, can run in any context,
+                regardless of whether the V8 engine is supported. 4. A specific
+                non-blocking operation is required to access any operating
+                system. There are a few essential objects in JavaScript, but
+                they are entirely OS-specific. Node.js, on the other hand, can
+                now operate non-blocking software tasks out of any JavaScript
+                programming. It contains no OS-specific constants. Node.js
+                establishes a strong relationship with the system files,
+                allowing companies to read and write to the hard drive. 5. The
+                critical benefits of JavaScript include a wide choice of
+                interfaces and interactions and just the proper amount of server
+                contact and direct visitor input. Node.js, on the other hand,
+                offers node package management with over 500 modules and the
+                capacity to handle many requests at the same time. It also
+                offers the unique ability to enable microservice architecture
+                and the Internet of Things. Even when comparing node js vs.
+                react js, node js always wins.{" "}
+              </p>
             </details>
             <details className="w-full border rounded-lg">
               <summary className="px-4 py-6 focus:outline-none focus-visible:ring-blue-600">
