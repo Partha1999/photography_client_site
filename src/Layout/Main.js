91f360f37fb60f001cb0f8121da043396225b389
Partha1999 <impartha1999@gmail.com>
2022-12-22 21:57:20 +0600

    svg added

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -42,7 +42,14 @@ const Main = () => {
                         to="/"
                         className="flex items-center p-2 space-x-3 rounded-md"
                       >
-                        
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
                           <path
                             strokeLinecap="round"
                             strokeLinejoin="round"
